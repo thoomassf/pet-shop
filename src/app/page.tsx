a76948a25104548ace0161b09116eac1a84a9a64
Thomas Silva de Farias <thomasfarias19@gmail.com>
2025-10-25 00:07:32 -0300

    feat: implemeting date-picker and dynamic search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
 import { AppointmentForm } from '@/components/appointment-form/appointment-form';
+import { DatePicker } from '@/components/date-picker';
 import { PeriodSection } from '@/components/period-section';
 import { Button } from '@/components/ui/button';
 import { prisma } from '@/lib/prisma';
-import { groupAppointmentByPeriod, APPOINTMENT_DATA } from '@/utils';
+import { groupAppointmentByPeriod } from '@/utils';
+import { endOfDay, parseISO, startOfDay } from 'date-fns';
 
-export default async function Home() {
-  const appointments = await prisma.appointment.findMany();
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ date?: string }>;
+}) {
+  const { date } = await searchParams;
+  const selectedDate = date ? parseISO(date) : new Date();
+
+  const appointments = await prisma.appointment.findMany({
+    where: {
+      scheduleAt: {
+        gte: startOfDay(selectedDate),
+        lte: endOfDay(selectedDate),
+      },
+    },
+    orderBy: {
+      scheduleAt: 'asc',
+    },
+  });
 
   const periods = groupAppointmentByPeriod(appointments);
 
@@ -20,6 +39,14 @@ export default async function Home() {
             Aqui você pode ver todos os clientes e serviços agendados para hoje.
           </p>
         </div>
+
+        <div className="hidden md:flex items-center gap-4">
+          <DatePicker />
+        </div>
+      </div>
+
+      <div className="mt-3 mb-8 md:hidden">
+        <DatePicker />
       </div>
 
       <div className="pb-24 md:pb-0">
---
+++ src/components/date-picker/date-picker.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import { Calendar, ChevronDown, ChevronLeft, ChevronRight } from 'lucide-react';
+import { Button } from '../ui/button';
+import { Popover, PopoverTrigger } from '../ui/popover';
+import { useRouter, usePathname, useSearchParams } from 'next/navigation';
+import { useCallback, useEffect, useState } from 'react';
+import { addDays, format, isValid } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+
+export const DatePicker = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const dateParam = searchParams.get('date');
+
+  const getInitialDate = useCallback(() => {
+    if (!dateParam) return;
+
+    const [year, month, day] = dateParam.split('-').map(Number);
+    const parsedDate = new Date(year, month - 1, day);
+
+    if (!isValid(parsedDate)) return new Date();
+
+    return parsedDate;
+  }, [dateParam]);
+
+  const [date, setDate] = useState<Date | undefined>(getInitialDate);
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+
+  const updateURLWithDate = (selectedDate: Date | undefined) => {
+    if (!selectedDate) return;
+
+    const newParams = new URLSearchParams(searchParams.toString());
+    newParams.set('date', format(selectedDate, 'yyyy-MM-dd'));
+    router.push(`${pathname}?${newParams.toString()}`);
+  };
+
+  const handleNavigateDay = (days: number) => {
+    const newDate = addDays(date || new Date(), days);
+    updateURLWithDate(newDate);
+  };
+
+  useEffect(() => {
+    const newDate = getInitialDate();
+
+    if (date?.getTime() !== newDate?.getTime()) {
+      setDate(newDate);
+    }
+  }, [date, getInitialDate]);
+
+  return (
+    <div className="flex items-center gap-2">
+      <Button variant="outline" onClick={() => handleNavigateDay(-1)}>
+        <ChevronLeft className="h-4 w-4" />
+      </Button>
+
+      <Popover open={isPopoverOpen} onOpenChange={setIsPopoverOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            className="w-min[180px] justify-between text-left font-normal bg-transparent border-border-primary text-content-primary hover:bg-background-tertiary hover:border-border-secondary hover:text-content-primary focus-visible:ring-offset-0 focus-visible:ring-1 focus-visible:ring-border-brand focus:border-border-brand focus-visible:border-border-brand"
+          >
+            <div className="flex items-center gap-2">
+              <Calendar className="h-4 w-4 text-content-brand" />
+              {date ? (
+                format(date, 'PPP', { locale: ptBR })
+              ) : (
+                <span>Selecione uma data</span>
+              )}
+            </div>
+            <ChevronDown className="h-4 w-4 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+      </Popover>
+
+      <Button variant="outline" onClick={() => handleNavigateDay(1)}>
+        <ChevronRight className="h-4 w-4" />
+      </Button>
+    </div>
+  );
+};

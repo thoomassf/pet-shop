29cc7f996487ab6586b17cd6b74c253ffe86349e
Thomas Silva de Farias <thomasfarias19@gmail.com>
2025-10-25 00:23:30 -0300

    feat: implemeting navigation button to date-picker

--- src/components/date-picker/date-picker.tsx
+++ src/components/date-picker/date-picker.tsx
@@ -13,6 +13,7 @@ import { useRouter, usePathname, useSearchParams } from 'next/navigation';
 import { useCallback, useEffect, useState } from 'react';
 import { addDays, format, isValid } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
+import { NavigationButton } from './navigation-button';
 
 export const DatePicker = () => {
   const router = useRouter();
@@ -62,9 +63,12 @@ export const DatePicker = () => {
 
   return (
     <div className="flex items-center gap-2">
-      <Button variant="outline" onClick={() => handleNavigateDay(-1)}>
+      <NavigationButton
+        tooltipText="Dia anterior"
+        onClick={() => handleNavigateDay(-1)}
+      >
         <ChevronLeft className="h-4 w-4" />
-      </Button>
+      </NavigationButton>
 
       <Popover open={isPopoverOpen} onOpenChange={setIsPopoverOpen}>
         <PopoverTrigger asChild>
@@ -94,9 +98,12 @@ export const DatePicker = () => {
         </PopoverContent>
       </Popover>
 
-      <Button variant="outline" onClick={() => handleNavigateDay(1)}>
+      <NavigationButton
+        tooltipText="Próximo dia"
+        onClick={() => handleNavigateDay(1)}
+      >
         <ChevronRight className="h-4 w-4" />
-      </Button>
+      </NavigationButton>
     </div>
   );
 };
